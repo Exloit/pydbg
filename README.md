83eba96f77ae3a9b7bdfada6585d83831cd004b4
Ryan Sears <Fitblip@gmail.com>
2012-04-18 18:17:57 -0400

    Re-ordered to allow for a sane setup.py to install just that module

---
+++ README.md
@@ -0,0 +1,3 @@
+PyDBG is a python debugging library and is used in the Sulley Fuzzing Framework (https://github.com/OpenRCE/sulley).
+
+Written by Pedram Amini
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name = "pydbg",
+      description = 'Python Debugging Library',
+      author = 'Pedram Amini',
+      author_email = 'pedram.amini@gmail.com',
+      package_dir = {'':"."},
+      packages = ["pydbg"],
+      )
